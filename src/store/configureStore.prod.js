b074edc9316e2aa986e5106a563862e78c0dd336
oanylund <ole.a.nylund@gmail.com>
2018-03-27 17:35:51 +0200

    fixed missing prod store setting

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers";
+import clipboard from "../middleware/clipboardMiddleware";
 
-// const enhancer = compose(applyMiddleware(a, b, c));
+const enhancer = compose(applyMiddleware(clipboard));
 
-const configureStore = () => createStore(rootReducer);
+const configureStore = () => createStore(rootReducer, enhancer);
 
 export default configureStore;
